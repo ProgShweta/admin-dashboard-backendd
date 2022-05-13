47f1e4b0fa0e3d016e2b8abf23393ac5052ceaa9
Shweta <shwetachaudhary2017@gmail.com>
2022-05-13 15:47:29 +0530

    admin-dashboard-backendd

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const apiRoute = require('./route/route')
-
 port=8080;
-
 app.use(express.json(),cors())
-
 app.use('/app/user', apiRoute)
-
 dotenv.config()
-
-
 mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true})
 .then(()=>{
     console.log('connected to DB')
